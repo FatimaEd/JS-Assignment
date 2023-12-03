d2f31f8eb8b842d9df276acc738c47fe6477498d
FatimaEd <fati.zdroussi@gmail.com>
2023-12-03 23:00:33 +0200

    JS Assignment Module 4

---
+++ Module 4/Assignment-1/index_4-1.js
@@ -0,0 +1,12 @@
+
+    function searchTvSeries() {
+
+    const query = document.getElementById('query').value;
+    const apiLink = `https://api.tvmaze.com/search/shows?q=${encodeURIComponent(query)}`;
+
+    fetch(apiLink)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+})
+}
